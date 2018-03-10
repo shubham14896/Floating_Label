ba0e7fea92b298b40f4911684d22256620bb4e97
Shubham Gupta <shubham9032@gmail.com>
2018-03-10 20:01:24 +0530

    Add files via upload

---
+++ floatingLabel/floatingLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  floatingLabel
+//
+//  Created by Shubham Gupta on 9/5/17.
+//  Copyright © 2017 Shubham Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
